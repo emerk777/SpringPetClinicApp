4332e2af137edd256c09fc8193a021d2a0f2dfad
emerk777 <emerk777@yahoo.com>
2021-07-28 18:18:12 -0600

    Project Initialization

---
+++ InitializrSpringbootProject/src/main/java/com/example/Spring5PetClinicApp/Spring5PetClinicAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring5PetClinicApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5PetClinicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5PetClinicAppApplication.class, args);
+	}
+
+}
---
+++ InitializrSpringbootProject/src/test/java/com/example/Spring5PetClinicApp/Spring5PetClinicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring5PetClinicApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5PetClinicAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
